b393fa09459c16c3adc80ed78a9ca35394b4cd25
Francis Bulus <notchera@gmail.com>
2019-11-28 20:58:28 +0100

    installed eslint and creating user notification handlers

---
+++ functions/handlers/notifications.js
@@ -0,0 +1,65 @@
+const firebase = require("firebase");
+const { db } = require("../utils/admin");
+
+exports.postForm = async (req, res) => {
+  const { inputs, options } = req.body;
+  if (!inputs) {
+    return res.status(400).json({ error: "No questions in form" });
+  }
+
+  const newForm = {
+    userId: req.user.id,
+    inputs,
+    created: new Date().toISOString()
+  };
+
+  if (options) newForm.options = options;
+
+  try {
+    const inputResult = await db.collection("forms").add(newForm);
+    const output = newForm;
+
+    output.formId = inputResult.id;
+    return res.status(201).json(output);
+  } catch (error) {
+    res.status(500).json({ error: "something went wrong: " + error.message });
+  }
+};
+
+exports.createNotification = async (req, res) => {
+  const { notification } = req.body;
+  try {
+    const result = await db.collection("notifications").add(notification);
+
+    //id, formId, isRead, createdAt, creatorId
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: `Oops, something went wrong, buddy: ${error.message}` });
+  }
+};
+
+exports.fetchUserNotifications = async (req, res) => {
+  try {
+    const data = [];
+    const userNotifications = await db
+      .collection("notifications")
+      .orderBy("created", "desc")
+      .where({ userId: req.user.id })
+      .get();
+
+    userNotifications.forEach(async notification => {
+      data.push({
+          id = notification.id,
+          isRead: doc.data().isRead,
+          created: doc.data().created,
+          form_name: await getFormName(notification.data(),userId)
+      });
+    });
+    return res.status(200).json(data);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: `Oops, something went wrong, buddy: ${error.message}` });
+  }
+};
--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
-const functions = require('firebase-functions');
-const app = require('express')();
-const auth = require('./utils/auth');
-const cors = require('cors');
+const functions = require("firebase-functions");
+const app = require("express")();
+const auth = require("./utils/auth");
+const cors = require("cors");
 app.use(cors());
 
-const { login, signup, getUserDetails } = require('./handlers/users');
+const { login, signup, getUserDetails } = require("./handlers/users");
 const {
   postForm,
   getUserForms,
@@ -13,18 +13,22 @@ const {
   addResponse,
   getFormResponses,
   getFormById
-} = require('./handlers/forms');
+} = require("./handlers/forms");
 
-app.post('/signup', signup);
-app.post('/login', login);
+const { createNotification } = require("./handlers/notifications");
 
-app.get('/users', auth, getUserDetails);
-app.post('/forms', auth, postForm);
-app.get('/forms', auth, getUserForms);
-app.get('/forms/:id', getFormById);
-app.delete('/forms/:id', auth, deleteUserForm);
-app.get('/allForms', getAllForms);
-app.post('/forms/:id/responses', addResponse);
-app.get('/forms/:id/responses', getFormResponses);
+app.post("/signup", signup);
+app.post("/login", login);
+
+app.get("/users", auth, getUserDetails);
+app.post("/forms", auth, postForm);
+app.get("/forms", auth, getUserForms);
+app.get("/forms/:id", getFormById);
+app.delete("/forms/:id", auth, deleteUserForm);
+app.get("/allForms", getAllForms);
+app.post("/forms/:id/responses", addResponse);
+app.get("/forms/:id/responses", getFormResponses);
+
+app.post("/forms/:id/notifications", createNotification);
 
 exports.api = functions.https.onRequest(app);
